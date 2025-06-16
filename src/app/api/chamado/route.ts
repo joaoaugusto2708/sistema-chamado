e4dcf627e0779579d263295fe948ee5ddeabe08d
João Augusto <joaorodriguesjf@hotmail.com>
2025-06-16 20:23:40 -0300

    sistema de chamados

---
+++ src/app/api/chamado/route.ts
@@ -0,0 +1,20 @@
+// app/api/chamado/route.ts
+import { supabase } from '../../../../lib/supabase'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+  const data = await request.json()
+
+  const { error } = await supabase.from('chamados').insert([{
+    nome: data.nome,
+    motivo: data.motivo,
+    descricao: data.descricao,
+  }])
+
+  if (error) {
+    console.error(error)
+    return NextResponse.json({ ok: false, error }, { status: 500 })
+  }
+
+  return NextResponse.json({ ok: true })
+}
